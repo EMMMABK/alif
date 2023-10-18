7b4f740cc4443570c80b024a06e637e9693e0589
aiustudent <aiustudent@mail.ru>
2023-10-18 10:10:09 +0600

    add test-commit

--- authorization_func/tests.py
+++ authorization_func/tests.py
@@ -1,30 +1,26 @@
-from django.test import TestCase
-from django.core import mail
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from .models import User
 
-# Create your tests here.
 
-class OTPCodeTest(APITestCase):
-    def test_otp_code_sent_on_registration(self):
-        # Создайте данные для регистрации пользователя
-        registration_data = {
-            "email": "wiyoces635@cindalle.com",
-            "name": "Velag",
-            "surname": "Velag2",
-            "phone_number": "+123456789",
-            "password": "kopeika2",
-            "repeat_password": "kopeika2",  # Сделайте совпадающим с паролем
+class LoginTest(APITestCase):
+    def setUp(self):
+        # Создайте пользователя для тестирования входа
+        self.user = User.objects.create_user(
+            email="test@example.com",
+            password="mypassword"
+        )
+
+    def test_user_login(self):
+        # Данные для входа
+        login_data = {
+            "email": "test@example.com",
+            "password": "mypassword"
         }
 
-        # Сделайте запрос на регистрацию пользователя
-        response = self.client.post(reverse("register"), registration_data, format="json")
+        # Отправьте запрос на вход
+        response = self.client.post(reverse("login"), login_data, format="json")
 
-        # Проверьте, что регистрация прошла успешно (HTTP 201 Created)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-        # Проверьте, что внутри электронного письма есть OTP-код
-        self.assertEqual(len(mail.outbox), 1)  # Убедитесь, что было отправлено одно письмо
-        self.assertIn("Your OTP Code is:", mail.outbox[0].body)  # Проверьте наличие OTP-кода в тексте письма
+        # Проверьте, что вход выполнен успешно (HTTP 200 OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
